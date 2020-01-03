8db6211477ecd9e7599654a29d069697ffb04e59
Volodymyr Kovalenko <vokovalenko16@gmail.com>
2020-01-02 21:02:22 -0800

    Solidity

---
+++ contracts/indbox.sol
@@ -0,0 +1,12 @@
+
+contract Inbox {
+  string public message;
+
+    function Inbox(string initialMessage) public {
+      message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+      message = newMessage;
+    }
+}
